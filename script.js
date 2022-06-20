c6c4f16c4a7af382c63295385a6d348846d97460
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-06-20 20:29:31 +0300

    отображение картинок

--- script.js
+++ script.js
@@ -29,6 +29,18 @@ document.addEventListener("DOMContentLoaded",  () => {
 
     })
 
+    // отображение картинок карточки
+
+    cardList.addEventListener('click', (event) => {
+        const card = event.target.closest('.card')
+        const cardImage = card.querySelector('.card__image')
+        const productImageList = Array.from(card.querySelectorAll('.card__select-item img'))
+        const imageTarget = productImageList.filter(item => item === event.target )
+        if(imageTarget.length === 0) return
+        cardImage.src = event.target.src
+        console.log(event.target.src)
+    })  
+
     //валидация
     const name = modal.querySelector('input[name="name"]')
     const tel = modal.querySelector('input[name="tel"]')
@@ -39,7 +51,8 @@ document.addEventListener("DOMContentLoaded",  () => {
     // tel.pattern = "/+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}/"
     tel.value="+7(___)___-__-__"
     tel.addEventListener("input", mask, false);
-
+    
+    // отправка mail
     modalForm.addEventListener('submit', (event) => {
        
         event.preventDefault()
