32402c5dd3e2eb1cf8614966cd4032909918a8fb
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-06-21 17:54:46 +0300

    mail test

--- mail-send.php
+++ mail-send.php
@@ -1,14 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset"UTF-8 />
+    
+    <title>Ваше сообщение успешно отправлено</title>
+</script>
+
+</head>
+
+<body>
+
 <?php
 
 if(!empty($_GET['phone']) and !empty($_GET['mail'])){
+    var_dump($_GET)
     $name = trim(strip_tags($_GET['name']));
     $phone = trim(strip_tags($_GET['tel']));
     $product = trim(strip_tags($_GET['productItem']));
     
     
     $mailTo = 'work.alex966@gmail.com';
-
-    
     $mailTema = 'Заявка на покупку';
     $mailMassage = 'Заявку отправил: Телефон:'. $phone.'    '. 'Email:'. $mail . 'Товар:' . $product;
 
@@ -20,3 +31,5 @@ if(!empty($_GET['phone']) and !empty($_GET['mail'])){
 } 
 
 ?>
+
+</body>
